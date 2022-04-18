833cc7f7298abd51771fd0609f4c104b12d59c61
Niharika Dhulipala <54968212+Niharikamnit@users.noreply.github.com>
2022-04-19 00:54:35 +0530

    Added solution - LeetHub

--- Maximum Sum Bitonic Subsequence - GFG/maximum-sum-bitonic-subsequence.cpp
+++ Maximum Sum Bitonic Subsequence - GFG/maximum-sum-bitonic-subsequence.cpp
@@ -25,29 +25,42 @@ int main()
 
 int maxSumBS(int arr[] , int n )
 {
-     int MSIBS[n], MSDBS[n];
-     int max_sum=-1;
-    for (int i = 0; i < n; i++) {
-        MSDBS[i] = arr[i];
-        MSIBS[i] = arr[i];
-    }
- 
-    // Compute MSIBS values from left to right */
-    for (int i = 1; i < n; i++)
-        for (int j = 0; j < i; j++)
-            if (arr[i] > arr[j] && MSIBS[i] < MSIBS[j] + arr[i])
-                MSIBS[i] = MSIBS[j] + arr[i];
- 
-    // Compute MSDBS values from right to left
-    for (int i = n - 2; i >= 0; i--)
-        for (int j = n - 1; j > i; j--)
-            if (arr[i] > arr[j] && MSDBS[i] < MSDBS[j] + arr[i])
-                MSDBS[i] = MSDBS[j] + arr[i];
- 
-    // Find the maximum value of MSIBS[i] + MSDBS[i] - arr[i]
-    for (int i = 0; i < n; i++)
-        max_sum = max(max_sum, (MSDBS[i] + MSIBS[i] - arr[i]));
- 
-    // return max sum of bi-tonic sub-sequence
-    return max_sum;
+   if(n==1){
+       return arr[0];
+   }
+   
+   vector<int> ldp(n,0);
+   vector<int> rdp(n,0);
+   
+   for(int i=0;i<n;++i){
+       ldp[i]=arr[i];
+       int mx=0;
+       
+       for(int j=i-1;j>=0;--j){
+           if(arr[i]>arr[j]){
+               mx=max(mx,ldp[j]);
+           }
+       }
+       ldp[i]+=mx;
+   }
+   
+   for(int i=n-1;i>=0;--i){
+       rdp[i]=arr[i];
+       int mx=0;
+       for(int j=i;j<n;++j){
+           if(arr[i]>arr[j]){
+               mx=max(mx,rdp[j]);
+           }
+       }
+       rdp[i]+=mx;
+   }
+   
+   int ans=INT_MIN;
+   for(int i=0;i<n;++i){
+       int x=ldp[i]+rdp[i];
+       x-=arr[i];
+       ans=max(ans,x);
+   }
+   
+   return ans;
 }
