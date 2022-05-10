6e624e170c06b065ea5c93bd6fcdf57f061fd83e
Niharika Dhulipala <54968212+Niharikamnit@users.noreply.github.com>
2022-05-11 02:36:38 +0530

    Added solution - LeetHub

--- Maximize The Cut Segments - GFG/maximize-the-cut-segments.cpp
+++ Maximize The Cut Segments - GFG/maximize-the-cut-segments.cpp
@@ -11,42 +11,10 @@ class Solution
 {
     public:
     //Function to find the maximum number of cuts.
-    // int maximizeTheCuts(int n, int x, int y, int z)
-    // {
-    //     //Your code here
-    //     int dp[n+1];
-    //     memset(dp,INT_MIN,sizeof(dp));
-    //     dp[0]=0;
-        
-    //     int minimum;
-    //     if (x < y && x < z)
-    //         minimum = x;
-    //     if (y < x && y < z)
-    //         minimum = y;
-    //     if (z < y && z < x)
-    //         minimum = z;
-        
-    //     if (minimum != 0 && n % minimum == 0)
-    //         return (n / minimum);
-        
-        
-        // for(int i=1;i<=n;i++) {
-        //         if(i>=x)
-        //         dp[i]=max(dp[i],1+dp[i-x]);
-                
-        //         if(i>=y)
-        //         dp[i]=max(dp[i],1+dp[i-y]);
-                
-        //         if(i>=z)
-        //         dp[i]=max(dp[i],1+dp[i-z]);
-        // }
-    //     return dp[n]==INT_MIN ? -1 : dp[n];
-    // }
-    
-    
     int maximizeTheCuts(int n, int x, int y, int z)
-{
-    int minimum;
+    {
+        //Your code here
+        int minimum;
     if (x < y && x < z)
         minimum = x;
     if (y < x && y < z)
@@ -61,7 +29,8 @@ class Solution
     for (int i = 1; i <= n; i++)
         dp[i] = INT_MIN;
     dp[0] = 0;
-       for(int i=1;i<=n;i++) {
+        
+        for(int i=1;i<=n;i++) {
                 if(i>=x)
                 dp[i]=max(dp[i],1+dp[i-x]);
                 
@@ -71,8 +40,39 @@ class Solution
                 if(i>=z)
                 dp[i]=max(dp[i],1+dp[i-z]);
         }
-    return dp[n]<0 ? 0 : dp[n];
-}
+        return dp[n]<0 ? 0 : dp[n];
+    }
+    
+    
+//     int maximizeTheCuts(int n, int x, int y, int z)
+// {
+//     int minimum;
+//     if (x < y && x < z)
+//         minimum = x;
+//     if (y < x && y < z)
+//         minimum = y;
+//     if (z < y && z < x)
+//         minimum = z;
+
+//     if (minimum != 0 && n % minimum == 0)
+//         return (n / minimum);
+
+//     int dp[n + 1];
+//     for (int i = 1; i <= n; i++)
+//         dp[i] = INT_MIN;
+//     dp[0] = 0;
+//       for(int i=1;i<=n;i++) {
+//                 if(i>=x)
+//                 dp[i]=max(dp[i],1+dp[i-x]);
+                
+//                 if(i>=y)
+//                 dp[i]=max(dp[i],1+dp[i-y]);
+                
+//                 if(i>=z)
+//                 dp[i]=max(dp[i],1+dp[i-z]);
+//         }
+//     return dp[n]<0 ? 0 : dp[n];
+// }
 };
 
 // { Driver Code Starts.
