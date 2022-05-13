2ac01112e8e7775400389c01dc80e551fcbc409d
Niharika Dhulipala <54968212+Niharikamnit@users.noreply.github.com>
2022-05-13 12:27:03 +0530

    Time: 297 ms (37.64%), Space: 11.5 MB (55.60%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -59,15 +59,15 @@ public:
         // string longestPalindrome(string s) {
 	const int n = s.size();
 	if(n==0) return "";
-	int dp[n][n], maxlen =1 , left=0;// maxlen = 1 when only one word
+	int dp[n][n], maxlen =1 , left=0;// maxlen = 1 when only one letter
 	memset(dp,0,n*n*sizeof(int));
-	for(int i=0;i<n;++i){
-		dp[i][i] = 1;
-		for(int j=0;j<i;++j){
-			dp[j][i] = (s[j]==s[i]  && (i-j< 2 || dp[j+1][i-1]));
-			if(dp[j][i] && maxlen < i-j+1){
-				left = j;
-				maxlen = i-j+1;
+	for(int end=0;end<n;++end) {
+		dp[end][end] = 1;
+		for(int start=0;start<end;++start){
+			dp[start][end] = (s[start]==s[end]  && (end-start< 2 || dp[start+1][end-1]));
+			if(dp[start][end] && maxlen < end-start+1){
+				left = start;
+				maxlen = end-start+1;
 			}
 		}
 	}
